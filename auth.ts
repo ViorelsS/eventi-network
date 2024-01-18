d8c31d6329e01aa2746a3c2e852b445559846da2
Viorel Strogoteanu <viorelss@icloud.com>
2024-01-18 14:26:57 +0100

    2FA completed

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
 import { getUserById } from "@/data/user";
 import { UserRole } from "@prisma/client";
+import { getTwoFactorConfirmationByUserId } from "./data/two-factor-confirmation";
 
 export const {
   handlers: { GET, POST },
@@ -36,7 +37,18 @@ export const {
       // Prevent sign in without email verification
       if (!existingUser?.emailVerified) return false;
 
-      // TODO: Add 2FA check
+      if (existingUser.isTwoFactorEnabled) {
+        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+          existingUser.id
+        );
+
+        if (!twoFactorConfirmation) return false;
+
+        // Delete two factor confirmation for next sign in
+        await db.twoFactorConfirmation.delete({
+          where: { id: twoFactorConfirmation.id },
+        });
+      }
 
       return true;
     },
--- schemas/index.ts
+++ schemas/index.ts
@@ -13,6 +13,7 @@ export const LoginSchema = z.object({
   password: z.string().min(1, {
     message: "La password è obbligatoria",
   }),
+  code: z.optional(z.string()),
 });
 
 export const RegisterSchema = z.object({
